9a2a97fbfda792e9facae2841380b3b5e861f8ce
Stuart Leach <jstuartleach@gmail.com>
2024-07-16 11:23:17 -0400

    new dashboard

--- frontend/src/lib/components/layout/Header.svelte
+++
@@ -1,1 +0,0 @@
-<h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">New York Bail</h2>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,36 +1,30 @@
 <script>
 	import '../app.css';
-	import LawVisionIcon from '$lib/components/assets/Logo.svelte';
 	import InfoBanner from '$lib/components/containers/InfoBanner.svelte';
-	import Header from '$lib/components/layout/Header.svelte';
 </script>
 
 <div class="">
 	<nav class="sticky bg-zinc-950 w-full shadow-md">
 
-		<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<div class="mx-auto mx-4 px-4 sm:px-6 lg:px-8">
 			<div class="flex h-[4rem] items-center justify-between">
-				<div class="flex items-center">
-					<div class="flex-shrink-0">
+				<div class="flex items-center w-full justify-between">
+					<div class="flex-shrink-0 flex-row ">
 
-						<a href="/">
-							<div class="w-full ">
+						<div class="w-full ">
+							<a href="/">
 								<h1
 									class="font-bold t text-4xl tracking-[-.04em] bg-gradient-to-tr from-red-500 to-yellow-300 bg-clip-text font-bold text-transparent">
 									LawVision</h1>
-							</div>
-						</a>
+							</a>
+						</div>
 
 					</div>
 					<div class="hidden md:block">
 						<div class="ml-10 grid grid-cols-2 space-x-4 ">
 							<a
-								href="/"
-								class="rounded-md bg-zinc-950 px-3 py-2 text-sm font-medium text-white transition text-xl"
-							>Home</a>
-							<a
 								href="mailto:jstuartleach@gmail.com"
-								class="rounded-md bg-zinc-950 px-3 py-2 text-sm font-medium text-white transition text-xl"
+								class="rounded-md bg-zinc-950 px-3 py-2 text-sm font-medium text-white transition text-base"
 							>Contact</a
 							>
 						</div>
@@ -41,8 +35,6 @@
 	</nav>
 	<div>
 		<InfoBanner />
-
 		<slot />
-
 	</div>
 </div>
